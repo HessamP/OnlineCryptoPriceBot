50c7ea74c421df91c38a6de00c59687dc3682329
Hessam P <hessam.p@gmail.com>
2022-03-18 21:13:45 +0330

    handle upper case keys in api

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from telegram.ext import *  # python-telegram-bot ver 13.11 Author: Leandro Tole
 import datetime
 import requests
 import json
-
 import key
 
 updater = Updater(key.bot_token)
@@ -11,7 +10,8 @@ dispatcher = updater.dispatcher
 
 
 def symbol_to_id(symbol):
-    try:  # based on API some keys are lower and some are upper case so handle this with try and except
+    try:  # based on API some keys are lower case and some are upper, so handle this with try and except to resolve
+        # KeyError
         cleaned_symbol = str(symbol).strip().lower()
         symbol_url = "https://api.coingecko.com/api/v3/coins/list?include_platform=true"
         id_request = json.loads(requests.get(url=symbol_url).text)
@@ -19,6 +19,7 @@ def symbol_to_id(symbol):
         location = find[cleaned_symbol]
 
         coin_id = id_request[location]["id"]
+        print("id lower", coin_id)
 
         return coin_id
     except:
@@ -29,7 +30,7 @@ def symbol_to_id(symbol):
         location = find[cleaned_symbol]
 
         coin_id = id_request[location]["id"]
-
+        print("id upper", coin_id)
         return coin_id
 
 
@@ -42,6 +43,7 @@ def symbol_to_name(symbol):
         location = find[cleaned_symbol]
 
         coin_name = id_request[location]["name"]
+        print("name lower", coin_name)
 
         return coin_name
     except:
@@ -52,46 +54,70 @@ def symbol_to_name(symbol):
         location = find[cleaned_symbol]
 
         coin_name = id_request[location]["name"]
+        print("name upper", coin_name)
 
         return coin_name
 
 
 def rounded_thousands_seperator_usd(dollars):
-    try:
-        if str(dollars).lower().__contains__("e"):
-            return "$" + f"{dollars :.12f}"
-        else:
-            return "$" + f"{round(dollars, 5):,}"
-    except:
-        if str(dollars).upper().__contains__("e"):
-            return "$" + f"{dollars :.12f}"
-        else:
-            return "$" + f"{round(dollars, 5):,}"
+    if str(dollars).lower().__contains__("e"):
+        return "$" + f"{dollars :.12f}"
+    else:
+        return "$" + f"{round(dollars, 5):,}"
 
 
 def rounded_thousands_seperator_percent(percent):
-    return f"{round(percent, 2):,}" + "%"
+    try:
+        return f"{round(percent, 2):,}" + "%"
+    except:
+        "not provided"
 
 
 def get_price(message):
     symbol = str(message).strip().replace("$", "")
-    if 2 < len(symbol) <= 4:
-        versus = "usd"
-        coin_id = symbol_to_id(symbol)
-        coingecko_price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={versus}&include_market_cap" \
-                              "=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true "
+    versus = "usd"
+    coin_id = symbol_to_id(symbol)
+    coingecko_price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={versus}&include_market_cap" \
+                          "=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true "
 
-        price_request = json.loads(requests.get(url=coingecko_price_url).text)
-        price_output = price_request[coin_id]
-        price = rounded_thousands_seperator_usd(price_output["usd"])
-        usd_market_cap2 = rounded_thousands_seperator_usd(price_output["usd_market_cap"])
-        print(price)
+    price_request = json.loads(requests.get(url=coingecko_price_url).text)
+    price_output = price_request[coin_id]
+    price = rounded_thousands_seperator_usd(price_output["usd"])
+    usd_market_cap2 = rounded_thousands_seperator_usd(price_output["usd_market_cap"])
+    usd_24h_vol = rounded_thousands_seperator_usd(price_output["usd_24h_vol"])
 
-        usd_24h_change = rounded_thousands_seperator_percent(price_output["usd_24h_change"])
-        print(usd_24h_change)
+    usd_24h_change = rounded_thousands_seperator_percent(price_output["usd_24h_change"])
+    print(usd_24h_change)
 
-        return symbol.title() + " - " + symbol_to_name(
-            symbol) + "\n" + "Price: " + price + "\n" + "24h Change: " + usd_24h_change
+    return symbol.title() + " - " + symbol_to_name(
+        symbol) + "\n" + "Price: " + price + "\n" + "24h Change: " + str(usd_24h_change) + "\n" + "Market Cap: " + str(
+        usd_market_cap2) + "\n24h Volume: " + str(usd_24h_vol)
+
+
+# getting token price versus other token (not usd)
+
+# def get_non_usd_price(message):
+#     symbol = str(message).strip().replace("$", "")
+#     if symbol[-3:] in json.loads(requests.get("https://api.coingecko.com/api/v3/simple/supported_vs_currencies").text):
+#         coin_id = symbol_to_id(symbol[-len(symbol):-3])
+#         versus=symbol[len(symbol)-3:]
+#         print(versus)
+#         coingecko_price_url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin_id}&vs_currencies={versus}&include_market_cap" \
+#                               "=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true "
+#
+#         price_request = json.loads(requests.get(url=coingecko_price_url).text)
+#         price_output = price_request[coin_id]
+#         price = rounded_thousands_seperator_usd(price_output[symbol[-3:]])
+#         versus_market_cap2 = rounded_thousands_seperator_usd(price_output[f"{symbol[-3:]}_market_cap"])
+#         print(price)
+#
+#         versus_24h_change = rounded_thousands_seperator_percent(price_output[f"{symbol[-3:]}_24h_change"])
+#         print(versus_24h_change)
+#
+#         return symbol.title() + " - " + symbol_to_name(
+#             symbol) + "\n" + "Price: " + price + "\n" + "24h Change: " + versus_24h_change
+#     else:
+#         return "Not valid versus coin, use /help"
 
 
 def start_command(update: Update, context: CallbackContext):
@@ -112,7 +138,7 @@ def btc_price_command(update: Update, context: CallbackContext):
 
 def response(input_text):
     user_message = str(input_text).lower().strip()
-    if user_message.__contains__("$"):
+    if user_message.endswith("$"):
         print(user_message)
         return get_price(user_message)
 
